21dab46cfd485fc19f0ad5b15321e55aad24981b
kafe523 <kafe.chan1@gmail.com>
2022-02-06 00:57:13 +0800

    prototype for hpi modules

--- Module/Modules_Sandbox.lua
+++ Module/Modules_Sandbox.lua
@@ -1,6 +1,19 @@
 -- I know the code is bad, forgive me.
 
-local p ={}
+local p = {}
+
+local linkAssamble = {
+    "", -- 1
+    " ",
+    "[",
+    "]",
+    '<span class="">', -- 5
+    "</span>",
+    '<span class="plainlinks">',
+    '<span class="error" style="font-size:smaller;">' -- 8
+    
+}
+
 
 local iconTable = {
     ["bili-logo"] = "[[File:Bilibilitv-logo.png|20px|link=]]",
@@ -12,64 +25,185 @@ local iconTable = {
 }
 
 local prefixTable = {
-    {prefix = "https://www.bilibili.com/video/" , icon = iconTable["bili-logo"]},
-    {prefix = "https://www.instagram.com/tv/" , icon = iconTable["ig-logo"]},
-    {prefix = "" , icon = iconTable["ig-logo"]},
-    {prefix = "https://www.nicovideo.jp/watch/" , icon = iconTable["nico-logo"]},
-    {prefix = "https://piapro.jp/t/", icon = iconTable["piapro-logo"]},
-    {prefix = "" , icon = iconTable["tweets-logo"]},
-    {prefix = "https://twitter.com/" , icon = iconTable["tweets-logo"]},
-    {prefix = "https://t.co/" , icon = iconTable["tweets-logo"]},
-    {prefix = "https://youtu.be/" , icon = iconTable["youtube-logo"]}
-
+    ["bilibili"] = {prefix = "https://www.bilibili.com/video/" , icon = iconTable["bili-logo"]},
+    ["instagram"] = {prefix = "https://www.instagram.com/tv/" , icon = iconTable["ig-logo"]},
+    ["niconico"] = {prefix = "https://www.nicovideo.jp/watch/" , icon = iconTable["nico-logo"]},
+    ["piapro"] = {prefix = "https://piapro.jp/t/", icon = iconTable["piapro-logo"]},
+    ["twitter"] = {prefix = "https://twitter.com/" , prefix2 = "https://t.co/", ending1 = "/status/" , icon = iconTable["tweets-logo"]},
+    ["youtube"] = {prefix = "https://youtu.be/" , icon = iconTable["youtube-logo"]}
 }
+
+--[[
+    Index Start at 1
+]]
 
 local centralTable = {
     -- center
-    prefixTable[1], 
-    prefixTable[2],
-    prefixTable[3],
-    prefixTable[4],
-    prefixTable[5],
-    prefixTable[6],
-    prefixTable[7],
-    prefixTable[8],
-    prefixTable[9]
+    prefixTable["bilibili"], -- 1
+    prefixTable["instagram"],
+    prefixTable["instagram"],
+    prefixTable["niconico"],
+    prefixTable["piapro"], -- 5
+    prefixTable["twitter"],
+    prefixTable["twitter"],
+    prefixTable["twitter"],
+    prefixTable["youtube"] -- 9
+}
+
+local leftTable = {
     --left
+    [10] = prefixTable["bilibili"],
+    [11] = prefixTable["instagram"],
+    [12] = prefixTable["niconico"],
+    [13] = prefixTable["piapro"],
+    [14] = prefixTable["twitter"],
+    [15] = prefixTable["youtube"],
+}
+
+local rightTable = {
     --right
+    [16] = prefixTable["bilibili"],
+    [17] = prefixTable["instagram"],
+    [18] = prefixTable["niconico"],
+    [19] = prefixTable["piapro"],
+    [20] = prefixTable["twitter"],
+    [21] = prefixTable["youtube"],
+}
+
+local onlyLogoTable = {
+    [22] = prefixTable["bilibili"],
+    [23] = prefixTable["instagram"],
+    [24] = prefixTable["niconico"],
+    [25] = prefixTable["piapro"],
+    [26] = prefixTable["twitter"],
+    [27] = prefixTable["youtube"],
 }
 
 local indexTable = {
-    {"bl","bili","bilibili"},
+    -- Video Link
+    {"bl","bili","bilibili"},  -- 1
     {"ig","instagram"},
     {"igf","instagramfull"},
     {"nc","nico","niconico"},
-    {"pp","piapro"},
+    {"pp","piapro"}, -- 5
     {"tw","tweet","tweets"},
     {"twsc","tweetsc","twittershortcut"},
     {"tws","tweetsh","twittershort"},
-    {"yt","youtube"}
+    {"yt","youtube"}, -- 9
+    -- General Link (Logo on the left)
+    {"bl_l","blil_lf","bilibili_left"}, -- 10
+    {"ig_l","instagram_lf"},
+    {"nc_l","nico_lf","niconico_left"},
+    {"pp_l","piapro_left"},
+    {"tw_l","tweets_lf","twittersh_left"}, -- 11
+    {"yt_l","youtube_left"},
+    -- General Link (Logo on the right)
+    {"bl_r","blil_rg","bilibili_right"},
+    {"ig_r","instagram_rg"},
+    {"nc_r","nico_rg","niconico_right"}, -- 15
+    {"pp_r","piapro_right"},
+    {"tw_r","tweets_rg","twittersh_right"},
+    {"yt_r","youtube_right"},
+    -- General Link (Logo but button)
+    {"bl_c","blil_ct","bilibili_center"},
+    {"ig_c","instagram_ct"}, -- 20
+    {"nc_c","nico_ct","niconico_center"},
+    {"pp_c","piapro_center"},
+    {"tw_c","tweets_ct","twittersh_center"},
+    {"yt_c","youtube_center"} -- 24
 }
 
-function p.main(A,B)
-    local translatedHeader = findPrefixIndex(A)
-    if translatedHeader == false then
-        return "Don't have this prefix"
-    elseif centralTable[translatedHeader] then
-        return "{{plain link |url=" .. centralTable[translatedHeader].prefix .. B.. " " .. centralTable[translatedHeader].icon .. " }}"
+local errorMessageTable = {
+    ["missingArg_1"] = linkAssamble[8] .. "缺少第1參數,請確認是否已填妥所需資料." .. linkAssamble[6],
+    ["missingArg_2"] = linkAssamble[8] .. "缺少第2參數,請確認是否已填妥所需資料." .. linkAssamble[6],
+    ["missingArg_3"] =  linkAssamble[8] .."缺少第3參數,請確認是否已填妥所需資料." .. linkAssamble[6],
+    ["tooManyArg"] = linkAssamble[8] .."請刪除多餘參數,或檢查第1參數是否錯誤." .. linkAssamble[6]
+}
+
+
+function p.main(argA,argB,argC,argD)
+    if argD ~= nil then
+        return errorMessageTable["tooManyArg"]
+    else
+        return p._main(argA,argB,argC)
     end
 end
 
+function p._main(argA,argB,argC)
 
-function findPrefixIndex (inputValue)
-for i , x in pairs(indexTable) do
-    for j , y in pairs(x) do
-        if x[j] == inputValue then
-        return i
+    local translatedHeader = p.findPrefixIndex(argA)
+    
+    if translatedHeader == false then
+        -- Default Error Message
+        return linkAssamble[8] .. "模板網站代號變數錯誤：<code>" .. argA .. "</code>本模板暫不支持此網站鏈接." .. linkAssamble[6]
+    elseif argB == nil then
+        return errorMessageTable["missingArg_2"]
+    end
+    -- Video Link Section
+    if centralTable[translatedHeader] then
+        return p.videoLink(translatedHeader,argB,argC)
+    end
+    -- Side Logo Section
+    if (leftTable[translatedHeader] or rightTable[translatedHeader]) and argC == nil then
+        return errorMessageTable["missingArg_3"]
+    elseif leftTable[translatedHeader] then
+        return p.generalLink_leftLogo(translatedHeader,argB,argC)
+    elseif rightTable[translatedHeader] then
+        return p.generalLink_rightLogo(translatedHeader,argB,argC)
+    end
+    
+    -- Center Logo Section
+    if onlyLogoTable[translatedHeader] then
+        if argC ~= nil then
+            return errorMessageTable["tooManyArg"]
+        else
+            return p.generalLink_centralLogo(translatedHeader,argB)
         end
     end
+
 end
-return false
+
+function p.findPrefixIndex (argA)
+
+    for i , x in pairs(indexTable) do
+        for j , y in pairs(x) do
+            if x[j] == argA then
+                return i
+            end
+        end
+    end
+    return false
+
 end
+
+function p.videoLink(translatedHeader,argB,argC)
+
+    if translatedHeader == 7 then -- twittershortcut
+        if argC == nil then
+            return errorMessageTable["missingArg_3"]
+        else
+            return linkAssamble[7] .. linkAssamble[3] .. centralTable[translatedHeader].prefix .. argB .. centralTable[translatedHeader].ending1 .. argC .. centralTable[translatedHeader].icon .. linkAssamble[4] .. linkAssamble[6]
+        end
+    elseif translatedHeader == 8 then
+        return linkAssamble[7] .. linkAssamble[3] .. centralTable[translatedHeader].prefix2 .. argB .. centralTable[translatedHeader].icon .. linkAssamble[4] .. linkAssamble[6]
+    elseif argC ~= nil then
+        return errorMessageTable["tooManyArg"]
+    end
+    return linkAssamble[7] .. linkAssamble[3] .. centralTable[translatedHeader].prefix .. argB .. linkAssamble[2] .. centralTable[translatedHeader].icon .. linkAssamble[4] .. linkAssamble[6]
+
+end
+
+function p.generalLink_leftLogo(translatedHeader,argB,argC)
+    return leftTable[translatedHeader].icon .. linkAssamble[3] .. argB .. linkAssamble[2] .. argC .. linkAssamble[4]
+end
+
+function p.generalLink_rightLogo(translatedHeader,argB,argC)
+    return linkAssamble[3] .. argB .. linkAssamble[2] .. argC .. rightTable[translatedHeader].icon .. linkAssamble[4]
+end
+
+function p.generalLink_centralLogo(translatedHeader,argB)
+    return linkAssamble[7] .. argB .. linkAssamble[3] .. onlyLogoTable[translatedHeader].icon .. linkAssamble[6]
+end
+
 
 return p
--- Module/Sandbox.lua
+++ Module/Sandbox.lua
@@ -1,7 +1,63 @@
 M = require("Module.Modules_Sandbox")
 
-print(M.main("yt","ABC"))
-print(M.main("pp","321"))
+--[[
+
+Sandbox for debugging Modules_Sandbox
+
+Template:
+print(M.main("","LINK"))
+]]
+
+print(M.main("bl","LINK"))
+print(M.main("ig","LINK"))
+print(M.main("igf","LINK"))
+print(M.main("nc","LINK"))
+print(M.main("pp","LINK"))
+print(M.main("tw","LINK"))
+print(M.main("twsc","PART2","PART3"))
+print(M.main("tws","LINK"))
+print(M.main("yt","LINK"))
+
 print(M.main("randomStuff"))
 
 
+print("------------------")
+
+print(M.main("bl_l","LINK","NAME"))
+print(M.main("ig_l","LINK","NAME"))
+print(M.main("nc_l","LINK","NAME"))
+print(M.main("pp_l","LINK","NAME"))
+print(M.main("tw_l","LINK","NAME"))
+print(M.main("yt_l","LINK","NAME"))
+
+print(M.main("pp_l"))
+print(M.main("tw_l","LINK"))
+print(M.main("yt_l","LINK","NAME","LOREM"))
+print(M.main("randomStuff"))
+
+print("------------------")
+
+print(M.main("bl_r","LINK","NAME"))
+print(M.main("ig_r","LINK","NAME"))
+print(M.main("nc_r","LINK","NAME"))
+print(M.main("pp_r","LINK","NAME"))
+print(M.main("tw_r","LINK","NAME"))
+print(M.main("yt_r","LINK","NAME"))
+
+print(M.main("pp_r"))
+print(M.main("tw_r","LINK"))
+print(M.main("yt_r","LINK","NAME","LOREM"))
+print(M.main("randomStuff"))
+
+print("------------------")
+
+print(M.main("bl_c","LINK"))
+print(M.main("ig_c","LINK"))
+print(M.main("nc_c","LINK"))
+print(M.main("pp_c","LINK"))
+print(M.main("tw_c","LINK"))
+print(M.main("yt_c","LINK"))
+
+print(M.main("pp_c"))
+print(M.main("yt_c","LINK","NAME","LOREM"))
+print(M.main("randomStuff"))
